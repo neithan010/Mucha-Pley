2bc792b3da388e66b6893c7cc9e7a02081a105fd
franss22 <franortizz2001@gmail.com>
2021-09-03 23:01:16 -0400

    Shaders y Brillibrilli

--- Keep Going Down/Minion/Minion.gd
+++ Keep Going Down/Minion/Minion.gd
@@ -5,23 +5,33 @@ const MAX_SPEED := 30000
 const FRICTION := 3000
 const ACCEL := 4000
 
+const INPUT_NAME := "minion"
+const UP    = INPUT_NAME+"_up"
+const DOWN  = INPUT_NAME+"_down"
+const RIGHT = INPUT_NAME+"_right"
+const LEFT  = INPUT_NAME+"_left"
+
+
 var velocity := Vector2.ZERO
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 func _physics_process(delta):
 	var input_vector := Vector2.ZERO
-	input_vector.x = Input.get_action_strength("minion_right")-Input.get_action_strength("minion_left")
-	input_vector.y = Input.get_action_strength("minion_down")-Input.get_action_strength("minion_up")
+	input_vector.x = Input.get_action_strength(RIGHT) - Input.get_action_strength(LEFT)
+	input_vector.y = Input.get_action_strength(DOWN)  - Input.get_action_strength(UP)
 	
 	if input_vector != Vector2.ZERO:
 		velocity += input_vector * ACCEL * delta
 		velocity = velocity.clamped(MAX_SPEED * delta)
+		rotation = velocity.angle()+PI/2
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_slide(velocity)
-	
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
