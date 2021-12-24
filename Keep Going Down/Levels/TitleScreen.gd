2f6f6e3cdd25d4257e29eab8610b370d757766e0
Cleparedes <clparede@dcc.uchile.cl>
2021-12-23 23:52:52 -0300

    Added credits screen

--- Keep Going Down/Levels/TitleScreen.gd
+++ Keep Going Down/Levels/TitleScreen.gd
@@ -1,14 +1,20 @@
 extends CanvasLayer
 
 onready var res
+onready var menu = $Menu
+onready var credits = $Credits/RichTextLabel
 onready var play_button = $Menu/Play
 onready var file_button = $Menu/File
 onready var quit_button = $Menu/Quit
+onready var credit_button = $Menu/Credit
+onready var close_button = $Credits/Close
 
 func _ready():
 	play_button.connect("pressed", self, "on_play_pressed")
 	file_button.connect("pressed", self, "on_file_pressed")
 	quit_button.connect("pressed", self, "on_quit_pressed")
+	credit_button.connect("pressed", self, "on_credit_pressed")
+	close_button.connect("pressed", self, "on_close_pressed")
 
 # TODO: debug buttons after returning from Archive.tscn
 func on_play_pressed():
@@ -21,3 +27,13 @@ func on_file_pressed():
 
 func on_quit_pressed():
 	get_tree().quit()
+
+func on_credit_pressed():
+	menu.visible = false
+	credits.visible = true
+	close_button.visible = true
+
+func on_close_pressed():
+	credits.visible = false
+	close_button.visible = false	
+	menu.visible = true
